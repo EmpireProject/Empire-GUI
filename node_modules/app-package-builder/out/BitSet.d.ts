c8bf624596d60249843c2db667a90b483c43d021
Alex Flores <domain@me.com>
2018-04-23 14:46:59 -0400

    remove replace node_modules with package manifest

--- node_modules/app-package-builder/out/BitSet.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class BitSet {
-    private readonly words;
-    constructor(nbits: number);
-    clear(bitIndex: number): number;
-    set(bitIndex: number): number;
-    get(bitIndex: number): boolean;
-    cardinality(): number;
-}
--- node_modules/builder-util/out/log.d.ts
+++
@@ -1,5 +0,0 @@
-import BluebirdPromise from "bluebird-lst";
-export declare function setPrinter(value: ((message: string) => void) | null): void;
-export declare function warn(message: string): void;
-export declare function log(message: string): void;
-export declare function task(title: string, promise: BluebirdPromise<any> | Promise<any>): BluebirdPromise<any>;
